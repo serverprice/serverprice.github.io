58c61a04acf2f0286c7b5ce9ae69bbc1f2f19749
serverprice.io <serverprice@gmail.com>
2020-02-12 18:00:15 +0800

    add fetch script

---
+++ README.md
@@ -0,0 +1,11 @@
+# server price
+
+best price for cloud server
+
+# how it works?
+
+1. fetch price info from server provider
+
+2. generate posts
+
+3. keep updating
---
+++ spider/fetch.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import Writer from "./writer";
+import fs = require("fs");
+import path = require("path");
+const url = `https://a0.p.awsstatic.com/pricing/1.0/ec2/region/us-east-1/ondemand/linux/index.json?timestamp=${+new Date()}`;
+
+axios
+  .get(url)
+  .then(function(response) {
+    const w = new Writer("Amazon AWS Server Price");
+    w.writeText("aws server price");
+    let data = [];
+    response.data.prices.forEach(e => {
+      data.push([
+        e["attributes"]["aws:ec2:instanceType"],
+        e["attributes"]["aws:ec2:vcpu"],
+        e["attributes"]["aws:ec2:memory"],
+        e["price"]["USD"]
+      ]);
+    });
+    data = data.sort((l, r) => {
+      if (l[0] > r[0]) {
+        return 1;
+      }
+      return -1;
+    });
+    w.writeTable(
+      ["instance", "cpu", "memory", "price"],
+      ["", "", "", ""],
+      data
+    );
+
+    fs.writeFile(
+      path.join(__dirname, "../source/_posts/dynamic/aws_server_price.md"),
+      w.getContent(),
+      function(err) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("File created!");
+      }
+    );
+  })
+  .catch(function(error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function() {
+    // always executed
+  });
